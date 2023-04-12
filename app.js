6d58147ba96725a031bea501047c78723a53e63a
Catalina Larion <larion.catalina@gmail.com>
2023-04-12 15:46:18 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,94 @@
+// const squares = document.querySelectorAll(".square");
+// const mole = document.querySelector(".mole");
+// const timeLeft = document.querySelector("#time-left");
+// const score = document.querySelector("#score");
+
+// let result = 0;
+// let hitPosition;
+// let currentTime = 60;
+// let timeId = null;
+
+// function randomSquare() {
+//   squares.forEach((square) => {
+//     square.classList.remove("mole");
+//   });
+
+//   let randomSquare = squares[Math.floor(Math.random() * 9)];
+//   randomSquare.classList.add("mole");
+//   hitPosition.classList.add("mole");
+// }
+// squares.forEach(function (square) {
+//   square.addEventListener("mousedown", () => {
+//     if (square.id == hitPosition) {
+//       result++;
+//       score.textContent = result;
+//       hitPosition = null;
+//     }
+//   });
+// });
+// function moveMole() {
+//   timeId = setInterval(randomSquare, 500);
+// }
+// moveMole();
+
+// function countDown() {
+//   currentTime--;
+//   timeLeft.textContent = currentTime;
+//   if (currentTime == 0) {
+//     clearInterval(countDownTimerId);
+//     clearInterval(timeId);
+//     alert("Game over. Your final score is:" + result);
+//   }
+// }
+
+// let countDownTimerId = setInterval(countDown, 1000);
+
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const timeLeft = document.querySelector("#time-left");
+const score = document.querySelector("#score");
+
+let result = 0;
+let hitPosition;
+let currentTime = 60;
+let timerId = null;
+
+function randomSquare() {
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+
+  let randomSquare = squares[Math.floor(Math.random() * 9)];
+  randomSquare.classList.add("mole");
+
+  hitPosition = randomSquare.id;
+}
+
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+      hitPosition = null;
+    }
+  });
+});
+
+function moveMole() {
+  timerId = setInterval(randomSquare, 2000);
+}
+
+moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime == 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert("winnnnnn! Your final score is " + result);
+  }
+}
+
+let countDownTimerId = setInterval(countDown, 1000);
